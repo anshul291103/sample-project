14a447671501aef177dcf3c358473543639feb3f
Anshul Pradeep <anshul.pradeep@ibm.com>
2026-02-01 18:38:42 +0530

    added get function and database

--- src/app.py
+++ src/app.py
@@ -2,9 +2,22 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("base.html")
+db = [
+    {"id": 1, "name": "John Doe", "Department": "Software", "Role": "Intern"},
+    {"id": 2, "name": "Alina", "Department": "Software", "Role": "Senior Developer"}
+]
+
+@app.route('/get/<id>')
+def get(id):
+    value = None
+    for val in db:
+        if val['id'] == int(id):
+            value = val
+
+    if value:
+        return value
+    else:
+        return f"No record with id {id}"
 
 if __name__ == '__main__':
     app.run(debug=True)
